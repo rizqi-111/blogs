d0bdc44c86826b434468364a999c7a7843ea5782
Mac <mac@Macs-MacBook-Pro.local>
2020-10-13 16:58:40 +0700

    migration

---
+++ app/Blog.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    //
+    protected $fillable = [
+        'title', 'content', 'user_id'
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+}
--- database/migrations/2020_10_13_094255_create_blogs_table.php
+++ database/migrations/2020_10_13_094255_create_blogs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBlogsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role',[0,1]);
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('content');
             $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('blogs');
     }
 }
